2305ad863baa93ecf6e91f7c67c298dc9358491a
Ashish Kumar <asgupta1910@gmail.com>
2025-05-29 12:36:36 +0530

    29/5/25

--- subAry.java
+++ subAry.java
@@ -4,7 +4,7 @@ public class subAry {
         int arr[]={2,4,6,8,10};
         int n=5;
         // int totalSubary=0;
-        // int currSum=0;
+        // int currSum = 0;
         for(int i=0; i<n;i++)
         {
             for(int j=i;j<n;j++)
@@ -15,7 +15,7 @@ public class subAry {
                     currSum+= arr[k]; //finds the sum of the subArray
                     System.out.print(arr[k]+" ");
                 }
-                // System.out.println(currSum);//prints the sum of the subArray
+                System.out.println(currSum);//prints the sum of the subArray
                 System.out.println();
                 // totalSubary++;
             }
